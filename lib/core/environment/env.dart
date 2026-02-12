6cf6ddd2c33b0fb913bd4fcaa6c2633fe7cb4095
Ngo Quoc Tu <ngotu2102@gmail.com>
2026-02-13 00:32:16 +0700

    update env

--- lib/core/environment/env.dart
+++ lib/core/environment/env.dart
@@ -1,9 +1,8 @@
 import 'package:envied/envied.dart';
-
 part 'env.g.dart';
 
 @Envied(path: '.env')
 abstract class Env {
-  @EnviedField(varName: 'API-KEY', obfuscate: true)
+  @EnviedField(varName: 'API_KEY', obfuscate: true)
   static final String apiKey = _Env.apiKey;
 }
